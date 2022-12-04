d6740546c62a5af3d6ce623df43d7abaaaff6f2f
“naandalist” <listiananda.apriliawan@gmail.com>
2022-12-04 22:47:05 +0700

    feat: update root route

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,17 +5,14 @@ import KbbiController from "../controllers/KbbiController";
 const router = Router();
 
 router.get("/", function (req: Request, res: Response) {
+  console.log("req: ", req.url);
   res.status(200).json({
     message: "Selamat datang di Servis API KBBI",
     data: [
       {
         info: "Sumber data sepenuhnya dari kbbi.kemdikbud.go.id!😃😊",
+        author: "https://github.com/Naandalist",
         endpoint: "/entri/{frasa yang akan ditemukan}",
-        example: [
-          `${Config.baseUrl}/entri/investigasi`,
-          `${Config.baseUrl}/entri/peperangan`,
-          `${Config.baseUrl}/entri/berdagang`,
-        ],
       },
     ],
   });
